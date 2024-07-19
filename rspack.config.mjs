1f74383d06f4b2afa404f7e85b76f5e5f2640a61
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-19 07:22:34 +0200

    chore: initial commit

---
+++ rspack.config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import { resolve } from "node:path";
+
+/** @type {import("@rspack/core").Configuration} */
+export default {
+  target: "node",
+  mode: "production",
+  entry: "./src/index.ts",
+  output: {
+    path: resolve(import.meta.dirname, "dist"),
+    filename: "index.js",
+    library: {
+      type: "commonjs2",
+    },
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        loader: "builtin:swc-loader",
+        options: {
+          jsc: {
+            parser: {
+              syntax: "typescript",
+            },
+          },
+        },
+        type: "javascript/auto",
+      },
+    ],
+  },
+  optimization: {
+    minimize: false,
+  },
+};
